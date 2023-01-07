12afa1cd094e763fe7460bcef23b151f7380cc9d
twoosky <lhjae3@naver.com>
2023-01-07 16:28:34 +0900

    feat: Post controller 작성

--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,8 +1,16 @@
 package com.example.mvc.model.http
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
+// dto 자체에 JSON 변수명 규칙 지정
+// @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest (
     var name: String? = null,
     var age: Int? = null,
     var email: String? = null,
-    var address: String? = null
+    var address: String? = null,
+
+    // JSON 변수명 지정 어노테이션
+    @JsonProperty("phone_number")
+    var phoneNumber: String? = null
 )
