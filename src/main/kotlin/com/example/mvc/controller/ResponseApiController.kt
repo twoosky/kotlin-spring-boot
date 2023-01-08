28c695321cbccab450af14649bff1009d760e8ca
twoosky <lhjae3@naver.com>
2023-01-08 23:43:32 +0900

    feat: ResponseApiController에 Validation 적용

--- src/main/kotlin/com/example/mvc/controller/ResponseApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/ResponseApiController.kt
@@ -1,8 +1,12 @@
 package com.example.mvc.controller
 
 import com.example.mvc.model.http.UserRequest
+import org.jetbrains.annotations.NotNull
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,9 +16,12 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+import javax.validation.constraints.Min
 
 @RestController
 @RequestMapping("/api/response")
+//@Validated
 class ResponseApiController {
 
     // RequestParam 의 required 속성을 통해 해당 파라미터 필수 여부를 지정할 수 있다. default true(필수)
@@ -44,9 +51,48 @@ class ResponseApiController {
         */
     }
 
+    // 1. bean이 아닌 곳에 validation 적용하기
+    // - age 파라미터는 bean이 아니기 때문에 클래스에 @Validated 를 붙여줘야 한다.
+    @GetMapping("/validation")
+    fun getMappingValidated(
+        @RequestParam name: String?,
+
+        @NotNull
+        @Min(value = 20, message = "age는 20보다 커야 합니다.")
+        @RequestParam age: Int
+    ): ResponseEntity<String> {
+        println(name)
+        println(age)
+        return ResponseEntity.status(200).body("${name}, ${age}")
+    }
+
     @PostMapping
     fun postMapping(@RequestBody userRequest: UserRequest?): ResponseEntity<UserRequest> {
         return ResponseEntity.status(200).body(userRequest)
+    }
+
+    // 2. bean에 대해 validation 적용하기
+    @PostMapping("/validation")
+    fun postMappingValidation(@Valid @RequestBody userRequest: UserRequest?): ResponseEntity<UserRequest> {
+        return ResponseEntity.status(200).body(userRequest)
+    }
+
+    // 3. validation 결과를 bindingResult 에 담아 에러 핸들링하기
+    // - 주의: @Validated 어노테이션 사용 시 bindingResult 에 validation 결과가 담기지 않는다.
+    @PostMapping("/validation2")
+    fun postMappingValidation2(@Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult): ResponseEntity<String> {
+        if (bindingResult.hasErrors()) {
+            val message = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val msg = it.defaultMessage
+                println("${field.field} : ${msg}")
+                message.append("${field.field} : ${msg}\n")
+            }
+            println(message.toString())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message.toString())
+        }
+        return ResponseEntity.status(200).body("OK")
     }
 
     @PutMapping
--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,16 +1,43 @@
 package com.example.mvc.model.http
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.constraints.*
 
 // dto 자체에 JSON 변수명 규칙 지정
 // @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest (
+
+    // kotlin에서 변수에 validation 관련 어노테이션 사용 시 @field 를 꼭 붙여줘야 한다.
+    @field:NotEmpty
+    @field:Size(min = 2, max = 8)
     var name: String? = null,
+
+    @field:PositiveOrZero  // 0보다 큰 수 즉, 양수인지 검증
     var age: Int? = null,
+
+    @field:Email
     var email: String? = null,
+
+    @field:NotBlank
     var address: String? = null,
 
     // JSON 변수명 지정 어노테이션
     @JsonProperty("phone_number")
-    var phoneNumber: String? = null
-)
+    @field:Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}\$")
+    var phoneNumber: String? = null,
+
+    var createdAt: String? = null  // yyyy-MM-dd HH:mm:ss
+){
+    // 검증 과정에서 메소드가 실행될 수 있도록 해주는 어노테이션
+    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss 여야 합니다.")
+    private fun isValidCreatedAt(): Boolean {
+        return try {
+            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
